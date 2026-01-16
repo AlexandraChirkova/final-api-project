be1c533afb63fd1462a7528d45de9549b8f31ac1
alexandra <alexandra.chirkova@kaspi.kz>
2026-01-16 09:35:13 +0500

    add api test

--- src/test/java/tests/api/LoginApiTest.java
+++ src/test/java/tests/api/LoginApiTest.java
@@ -5,6 +5,7 @@ import io.qameta.allure.Description;
 import models.LoginResponse;
 import models.UserCredentials;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
@@ -12,6 +13,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static specs.Specs.requestSpec;
 import static specs.Specs.responseSpec;
 
+@Tag("api")
 public class LoginApiTest extends BaseApiTest{
 
     @Test
